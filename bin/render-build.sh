0668080c56f4dd7cab27103c70fbae02a579b3c6
chloe050627 <hochitung0627@gmail.com>
2026-02-23 20:41:51 +0800

    .sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Exit immediately if any command fails
-set -e
+set -o errexit
 
 # Install gems
-bundle install --jobs 4 --retry 3
+bundle install 
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
 # Skip asset precompilation if not needed (Hello World has no real assets)
 # If you add JS/CSS later, uncomment: bundle exec rails assets:precompile
